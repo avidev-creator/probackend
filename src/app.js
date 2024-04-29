89f4455e5fd7d8307f415d305a31e41fb2bb96fa
avidev-creator <avidub371@gmail.com>
2024-04-29 22:10:33 +0530

    HTTP

--- src/app.js
+++ src/app.js
@@ -19,4 +19,11 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+
+// Routes--
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/users", userRouter)
+
 export { app };
