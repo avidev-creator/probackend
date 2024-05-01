bc3e90bb34d59d671bc310cbc708a46fba9ff056
avidev-creator <avidub371@gmail.com>
2024-05-01 19:59:48 +0530

    complete User registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,8 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 const registerUser = asyncHandler( async (req, res) => {
@@ -22,6 +25,9 @@ const registerUser = asyncHandler( async (req, res) => {
   const { fullName, email, username, password } = req.body
   
   console.log(email);
+  console.log(fullName);
+  console.log(username);
+  console.log(password);
 
   //if(fullName === ""){
   //  throw new ApiError(400, "got full name")
@@ -32,6 +38,49 @@ const registerUser = asyncHandler( async (req, res) => {
   ){
     throw new ApiError(400, "All fields required")
   }
+  
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }]
+  })
+  
+  if(existedUser){
+    throw new ApiError(409,"User Already Exists. Please login")
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+
+  const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+  if(!avatarLocalPath){
+    throw new ApiError(400, "Avatar File Required")
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if(!avatar){
+    throw new ApiError(400, "Avatar File Required")
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase()
+  })
+
+  const createdUser = await User.findById(user._id).select("-password -refreshToken")
+
+  if(!createdUser){
+    throw new ApiError(500, "Unable to create user. Please try again")
+  }
+
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "User Registered Sucessfully")
+  )
 
 })
 
